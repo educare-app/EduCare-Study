aa7f4b92e6145aaf3f8276a796db7542432e1c62
laxmankrishnamurti <freelancing.laxman@gmail.com>
2025-01-04 15:38:00 +0530

    Working on header section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,50 @@
+import { Link, NavLink } from "react-router-dom"
+import EduCareLogo from "../../assets/logo/EduCareLogo.png"
+import "./css/header.css"
+
 function Header() {
   return (
-    <div>This is header section</div>
+    <div className="section-one-wrapper">
+      <div className="section-one">
+        <header className="header-wrapper">
+          <div className="header-options">
+              <Link to='/'>
+                <div id="header-logo">
+                      <img src={EduCareLogo} alt="home-logo" className="logo"/>
+                </div>
+              </Link>
+              <div className="header-menu">
+                <nav id="navbar">
+                    <div className="nav-links">
+                      <Link to='/' id="home">
+                        <NavLink to='/'>Home</NavLink>
+                      </Link>
+                    </div>
+                    <div className="nav-links">
+                      <Link to='/' id="home">
+                        <NavLink to='/'>About</NavLink>
+                      </Link>
+                    </div>
+                    <div className="nav-links">
+                      <Link to='/' id="home">
+                        <NavLink to='/'>Services</NavLink>
+                      </Link>
+                    </div>
+                    <div className="nav-links">
+                      <Link to='/' id="home">
+                        <NavLink to='/'>Career</NavLink>
+                      </Link>
+                    </div>
+                    
+                </nav>
+              </div>
+              <div className="header-login-wrapper">
+                <button id="header-login-btn">Login</button>
+              </div>
+          </div>
+        </header>
+      </div>
+    </div>
   )
 }
 
